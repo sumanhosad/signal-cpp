6ee6963b1db1dc7fb2263507d3fe30f5454f05c9
sumanhosad <sumanhosad98@gmail.com>
2025-02-02 12:12:51 +0530

    Sun Feb  2 12:12:51 PM IST 2025 key exchnage on func

--- x3dh.cpp
+++ x3dh.cpp
@@ -1,27 +1,49 @@
 #include "includes/x3dh.h"
-void print_hex(const unsigned char *data, size_t len) {
-  for (size_t i = 0; i < len; ++i) {
-    std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)data[i];
-  }
-  std::cout << std::dec << std::endl;
-}
+#include "includes/print_hex.h"
+#include <iostream>
 
 int main() {
+  // Initialize libsodium
   if (sodium_init() < 0) {
     std::cerr << "libsodium initialization failed!" << std::endl;
     return 1;
   }
 
-  unsigned char identity_private_A[crypto_scalarmult_BYTES],
-      identity_public_A[crypto_scalarmult_BYTES];
+  // Allocate buffers for keys and signatures
+  unsigned char identity_public[crypto_scalarmult_BYTES];
+  unsigned char identity_private[crypto_scalarmult_BYTES];
+  unsigned char spk_public[crypto_scalarmult_BYTES];
+  unsigned char spk_private[crypto_scalarmult_BYTES];
+  unsigned char
+      spk_signature[crypto_sign_BYTES]; // Signature uses different size
+  unsigned char opk_public[crypto_scalarmult_BYTES];
+  unsigned char opk_private[crypto_scalarmult_BYTES];
+  unsigned char ek_public[crypto_scalarmult_BYTES];
+  unsigned char ek_private[crypto_scalarmult_BYTES];
 
-  unsigned char identity_private_B[crypto_scalarmult_BYTES],
-      identity_public_B[crypto_scalarmult_BYTES];
+  // Create X3DH instance (identity keys)
+  X3DH x3dh(identity_public, identity_private);
 
-  X3DH alice(identity_public_A, identity_private_A);
-  X3DH bob(identity_public_B, identity_private_B);
-  std::cout << "Identity Private Key A: ";
-  print_hex(identity_private_A, crypto_scalarmult_BYTES);
-  std::cout << "Identity Public Key A: ";
-  print_hex(identity_public_A, crypto_scalarmult_BYTES);
-};
+  // Generate keys
+  x3dh.generate_signed_pre_keys(spk_public, spk_private, spk_signature,
+                                identity_private);
+  x3dh.generate_pre_keys(opk_public, opk_private);
+  x3dh.generate_ephemeral_keys(ek_public, ek_private);
+
+  // Print keys
+  std::cout << "Identity Public Key: ";
+  print_hex(identity_public, crypto_scalarmult_BYTES);
+
+  std::cout << "Signed Pre-Key Public: ";
+  print_hex(spk_public, crypto_scalarmult_BYTES);
+
+  std::cout << "One-Time Pre-Key Public: ";
+  print_hex(opk_public, crypto_scalarmult_BYTES);
+
+  std::cout << "Ephemeral Public Key: ";
+  print_hex(ek_public, crypto_scalarmult_BYTES);
+
+  std::cout << "X3DH keys generated successfully." << std::endl;
+
+  return 0;
+}
