19aa07962b54ab716a4289af0f0e6ba075bbebfc
sumanhosad <sumanhosad98@gmail.com>
2025-02-07 19:01:13 +0530

    Fri Feb  7 07:01:13 PM IST 2025 signal branch

--- includes/printHex.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_HEX_H
-#define PRINT_HEX_H
-
-#include <iomanip>
-#include <iostream>
-
-void print_hex(const unsigned char *data, size_t len);
-
-#endif
--- src/print_hex.cc
+++
@@ -1,8 +0,0 @@
-#include "../includes/print_hex.h"
-
-void print_hex(const unsigned char *data, size_t len) {
-  for (size_t i = 0; i < len; ++i) {
-    std::cout << std::hex << std::setw(2) << std::setfill('0') << (int)data[i];
-  }
-  std::cout << std::dec << std::endl;
-}
